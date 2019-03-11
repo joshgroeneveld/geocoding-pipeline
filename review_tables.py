1260db936ebf8b137c4433dc34099aa55ba914f1
Josh Groeneveld <joshua.a.groeneveld@gmail.com>
2019-03-11 15:50:28 -0600

    Load input Excel files into a pandas data frame to replace spaces in column names with underscores

--- review_tables.py
+++ review_tables.py
@@ -5,6 +5,9 @@ A module that reviews all of the input Excel files to check for easy-to-catch er
 '''
 
 import os
+import shutil
+import glob
+import pandas as pd
 
 def main(input_directory):
     print("Begin reviewing records")
@@ -17,12 +20,20 @@ def main(input_directory):
     else:
         os.mkdir('Facilities_to_Geocode')
 
+    facilities_to_geocode = os.path.join(os.getcwd(), 'Facilities_to_Geocode')
+    prep_excel_files(input_directory, facilities_to_geocode)
+
 
 # For each Excel file in <input_directory>, do the following:
-
-
-
-# Rename all of the column headers to replace spaces with underscores
+def prep_excel_files(input_directory, output_directory):
+    print("Prepping Excel files")
+    # Load the input Excel file into a pandas data frame
+    original_excel_files = glob.glob(input_directory + "\\*.xlsx", recursive=False)
+    for original_excel_file in original_excel_files:
+        df = pd.read_excel(original_excel_file)
+        # Rename all of the column headers to replace spaces with underscores
+        df.rename(columns=lambda x: x.replace(' ', '_'), inplace=True)
+        print(df.head())
 
 # Identify potential problem records
 
