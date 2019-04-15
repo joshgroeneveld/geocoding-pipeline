d4d866c5cea52931a98270e2d548adf9b0248e44
Josh Groeneveld <joshua.a.groeneveld@gmail.com>
2019-04-15 16:42:16 -0600

    Join the geocoded tables with the initial inputs and concatenate results with locations that have known lat / long coordinates

--- geocode_tables.py
+++ geocode_tables.py
@@ -10,6 +10,7 @@ import arcpy
 from api_key import API_KEY
 import glob
 import pandas as pd
+import numpy as np
 
 # Import the geocoding toolbox here so that it gets imported before switching directories
 arcpy.ImportToolbox('geocoding-toolbox-pro-python-3/TableGeocoder/AGRC Geocode Tools.tbx')
@@ -28,6 +29,7 @@ def main(directory_to_geocode):
         os.mkdir('Final_Output')
     final_output_directory = os.path.join(os.getcwd(), 'Final_Output')
     geocode_tables(directory_to_geocode, final_output_directory, geocoding_results)
+    join_geocode_results_with_inputs(directory_to_geocode, geocoding_results, final_output_directory)
 
 # For each table in <directory_to_geocode>, send all records through the geocoder
 def geocode_tables(directory_to_geocode, final_output_directory, geocoding_results_directory):
@@ -57,10 +59,43 @@ def geocode_tables(directory_to_geocode, final_output_directory, geocoding_resul
         # Rename the output CSVs to match the input Excel files
         for file in os.listdir(geocoding_results_directory):
             if file.startswith("GeocodeResults_"):
-                os.rename(file, file_root + "_geocoding_results.csv")
+                os.rename(os.path.join(geocoding_results_directory, file), os.path.join(geocoding_results_directory, file_root + "_geocoding_results.csv"))
+    print("Completed gecoding!")
 
-# Load the input Excel files and the output geocoded CSVs into separate Pandas data frames
+    # Join geocoding results with the original tables, concatentate the results with the known lat / long
+    # locations identified in the review step
+def join_geocode_results_with_inputs(directory_to_geocode, geocoding_results_directory, final_output_directory):
+    print("Joining inputs and outputs...")
+    # Load the input Excel files and the output geocoded CSVs into separate Pandas data frames
+    input_excel_files = glob.glob(directory_to_geocode + "\\*.xlsx", recursive=False)
+    for input_excel_file in input_excel_files:
+        excel_df = pd.read_excel(input_excel_file)
+        file_path = input_excel_file
+        file_name = file_path.split(sep='\\')
+        file_root = file_name[-1].split(sep='_to_')[0]
+        results_csv = glob.glob(geocoding_results_directory + "\\" + file_root + "*.csv", recursive=False)[0]
+        csv_df = pd.read_csv(results_csv)
 
-# Join the Excel data frame and the CSV data frame based on the input ID
+        # Join the Excel data frame and the CSV data frame based on the input ID
+        merge_df = pd.merge(excel_df, csv_df, how='inner', left_on='ID', right_on='INID')
 
-# Output the joined data frame to a new Excel file in the final output directory
+        # If there is a file with known lat / long locations identified in the review step,
+        # load them into another pandas dataframe and concatentate with the dataframe merged above
+        output_file_dir = final_output_directory
+        output_file_name = file_root + "_Geocode.xlsx"
+
+        results_latlong = glob.glob(geocoding_results_directory + "\\" + file_root + "_latlong.xlsx", recursive=False)
+        if len(results_latlong) == 1:
+            latlong_df = pd.read_excel(results_latlong[0])
+            latlong_df.assign(**{'INID': np.nan, 'INADDR': np.nan, 'INZONE': np.nan, 'MatchAddress': np.nan, 'Score': np.nan, 'XCoord': np.nan, 'YCoord': np.nan, 'Geocoder': np.nan})
+            concat_df = pd.concat([merge_df, latlong_df], sort=False)
+            concat_df.to_excel(os.path.join(output_file_dir, output_file_name), sheet_name="Sheet1")
+            print("Exported: " + str(output_file_name))
+
+        elif len(results_latlong) == 0:
+            merge_df.to_excel(os.path.join(output_file_dir, output_file_name), sheet_name="Sheet1")
+            print("Exported: " + str(output_file_name))
+
+        else:
+            print("There are more than two lat / long results with the same name...")
+    print("Completed joining inputs and outputs and exporting results!")
