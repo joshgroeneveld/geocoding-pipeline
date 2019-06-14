ab0a065f30d1a0b2d2d086fa9494ec972211b02f
Josh Groeneveld <joshua.a.groeneveld@gmail.com>
2019-06-14 15:34:44 -0600

    Create a placeholder for tests with some test data

---
+++ tests/test_geocoding-pipeline.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+# Does main geocoding-pipeline.py import other modules
+
+# Review tables
+# Will records with a blank address get identified as potential problems
+
+# Will records without a number in the address get identified as a potential problem
+
+# For records with errors, check inputs to update records
+# Will a new address be inserted into the data frame
+
+# Will a new lat long be inserted into the data frame
+
+# Will a record to be removed get dropped from the data frame
+
+# Will a set of geocoding results merge with input data
+
+# Will a merged df concatenate with a df of known lat / long locations
+
+# Will user input to verify results move files appropriately
+
+# Will records to be verified identify records with a low match score
+
+# Once verification is complete, will the unnecessary columns get dropped
